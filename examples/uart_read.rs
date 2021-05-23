fedba9e7328bbbcb8c24cac01f9957afd40339f6
Michael Emmert <michael.emmert@st.oth-regensburg.de>
2021-05-23 19:02:54 +0200

    read example with readline

--- examples/uart_read.rs
+++ examples/uart_read.rs
@@ -1,9 +1,11 @@
 #![no_std]
 #![no_main]
 
+use core::result::Result;
+
 use cortex_m::asm;
 use cortex_m_rt::entry;
-use hal::{pac::USART2, serial::Rx};
+use hal::time::MilliSeconds;
 use panic_halt as _;
 use stm32f3xx_hal::{self as hal, pac, prelude::*};
 use heapless::String;
@@ -16,8 +18,41 @@ use embedded_hal::serial::Read;
 const BAUDRATE: u32 = 9600;
 const BUFFERSIZE: usize = 100;
 
-fn read_string_until_timeout(delimiter: char, timeout: u32, rx: &mut impl Read<u8>){
+enum UART_Error{
+      Timeout,
+}
 
+fn read_string_until_timeout(delimiter: char, timeout: MilliSeconds, rx: &mut impl Read<u8>, timer: &hal::time::MonoTimer) -> Result<String<BUFFERSIZE>, UART_Error>{
+      let starttime = timer.now();
+      let max_time = timer.frequency().0 / 1000 * timeout.0; // convert timeout (ms) to ticks 
+      
+      let mut result = String::<BUFFERSIZE>::new();
+      let mut c: char;
+      loop{ 
+            // check if timeout
+            let current_time = starttime.elapsed();
+            if current_time > max_time{
+                  return Result::Err(UART_Error::Timeout);
+            }
+            // read character
+            let read_result = rx.read();
+            if read_result.is_err(){
+                  continue;
+            }
+            else{
+                  c = read_result.ok().unwrap() as char;
+                  if c == delimiter{
+                        break;
+                  }
+                  else{
+                        let append_res = result.push(c);
+                        if append_res.is_err(){
+                              return Result::Ok(result); // string is full (max. buffer size reached -> return the string)
+                        }
+                  }
+            }
+      }
+      Result::Ok(result)
 }
 
 /// read a string from the provided uart interface. Blocks until the delimiter is reached or the maximum buffer length is reached
@@ -73,6 +108,7 @@ fn read_line(rx: &mut impl Read<u8>) -> String<BUFFERSIZE>{
 #[entry]
 fn main() -> ! {
       let dp = pac::Peripherals::take().unwrap();
+      let cp = pac::CorePeripherals::take().unwrap();
 
       let mut rcc = dp.RCC.constrain();
       let mut flash = dp.FLASH.constrain();
@@ -87,11 +123,24 @@ fn main() -> ! {
       // create UART struct
       let uart = hal::serial::Serial::usart2(dp.USART2, (pa2_tx, pa3_rx), BAUDRATE.bps(), clocks, &mut rcc.apb1);
       // get tx and rx structs
-      let (mut _tx, mut rx) = uart.split();
+      let (mut tx, mut rx) = uart.split();
+
+      let mono_timer = hal::time::MonoTimer::new(cp.DWT, clocks);
       
       loop {
-            // let message = read_line(&mut rx);
-            let message = read_string_until('f', &mut rx);
-            asm::delay(10_000);
+            // echo lines
+            tx.bwrite_all("Send text!".as_bytes()).unwrap();
+            tx.bwrite_all("\r\n".as_bytes()).unwrap();
+            let line = read_string_until_timeout('\r', 10_000.ms(), &mut rx, &mono_timer); // delimiter may also be '\n', depends on the interacting program
+            match line{
+                  Ok(line) => {
+                        tx.bwrite_all("You sent: ".as_bytes()).unwrap();
+                        tx.bwrite_all(line.as_bytes()).unwrap();
+                  }
+                  Err(_error) => {
+                        tx.bwrite_all("Too slow, try again...".as_bytes()).unwrap();
+                  }
+            }
+            tx.bwrite_all("\r\n".as_bytes()).unwrap();
       }
 }
