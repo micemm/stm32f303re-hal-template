bccd5b9c6622f84282f4cc5995eb14d7c3ae0e63
Michael Emmert <michael.emmert@st.oth-regensburg.de>
2021-05-27 19:43:09 +0200

    added small project

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,103 @@
 #![no_std]
 #![no_main]
 
-use cortex_m::{asm, singleton};
-use cortex_m_rt::entry;
+mod i2c_led_bar;
+mod spi_oled_display;
+mod pwm_led;
+use i2c_led_bar::LEDBar;
+use spi_oled_display::OLEDDisplay;
+
 use panic_halt as _;
-use stm32f3xx_hal::{self as hal, pac, prelude::*};
 
-// write to uart using DMA
-// see also:
-// https://github.com/stm32-rs/stm32f3xx-hal/blob/v0.6.0/examples/serial_dma.rs
+use cortex_m::asm;
+use cortex_m_rt::entry;
 
-const TX_BUFFER_LENGTH: usize = 100;
+use stm32f3xx_hal::{self as hal, pac, prelude::*, delay::Delay};
+use embedded_hal::spi::Mode;
+use hal::spi::Spi;
+
+use heapless::String;
+
+const I2C_ADDR: u8 = 32;
 
 #[entry]
 fn main() -> ! {
-      let dp = pac::Peripherals::take().unwrap();
+    let mut dp = pac::Peripherals::take().unwrap();
+    let cp = pac::CorePeripherals::take().unwrap();
 
-      let mut rcc = dp.RCC.constrain();
-      let mut flash = dp.FLASH.constrain();
+    let mut flash = dp.FLASH.constrain();
+    let mut rcc = dp.RCC.constrain();
+    
+    let clocks = rcc
+        .cfgr
+        .sysclk(72u32.mhz())
+        .pclk1(36u32.mhz())
+        .freeze(&mut flash.acr);
 
-      let clocks = rcc.cfgr.freeze(&mut flash.acr);
+    let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
+    let mut gpiob = dp.GPIOB.split(&mut rcc.ahb);
 
-      let mut gpioa = dp.GPIOA.split(&mut rcc.ahb);
-      
-      // switch tx and rx pin to alternative function
-      let pa2_tx = gpioa.pa2.into_af7(&mut gpioa.moder, &mut gpioa.afrl);
-      let pa3_rx = gpioa.pa3.into_af7(&mut gpioa.moder, &mut gpioa.afrl);
+    // Configure I2C
+    let scl = gpiob.pb6.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
+    let sda = gpiob.pb7.into_af4(&mut gpiob.moder, &mut gpiob.afrl);
 
-      // create UART struct
-      let uart = hal::serial::Serial::usart2(dp.USART2, (pa2_tx, pa3_rx), 9600.bps(), clocks, &mut rcc.apb1);
-      // get tx and rx structs
-      let (mut tx, mut _rx) = uart.split();
-      
-      // create DMA struct
-      let dma1 = dp.DMA1.split(&mut rcc.ahb);
-      // dma channel for tx (number: see datasheet)
-      let mut tx_channel = dma1.ch7;
-      
-      // create DMA buffer for tx
-      // This buffer has to be a slice (&[u8]) with static lifetime. This con be achived by:
-      // 1) String literals: (Problem: only constant data can be written)
-      // let mut tx_slice = "testDMA\r\n".as_bytes();
-      
-      // 2) use the singleton!() macro to create a buffer of fixed length
-      // (note: make sure that this line is only executed once as it will not create a second buffer but return 'None' if it is called again)
-      // This creates an array of fixed size that can be used as buffer. 
-      // As the buffer is passed as slice (&[u8]) when transmitting data, it is also possible to write only parts of the array:
-      let tx_buffer = singleton!(: [u8; TX_BUFFER_LENGTH] = [0; TX_BUFFER_LENGTH]).unwrap();
+    let i2c = hal::i2c::I2c::new(
+        dp.I2C1,
+        (scl, sda),
+        100u32.khz(),
+        clocks,
+        &mut rcc.apb1,
+    );
 
-      // exapmle: write "Hello World!" using this buffer:
-      let bytes = "Hello world!\r\n".as_bytes();
-      // copy all values to the buffer:
-      // for (i, value) in bytes.iter().enumerate(){
-      //       tx_buffer[i] = *value; // copy the value to the buffer
-      // }
-      // or without loop:
-      tx_buffer[..bytes.len()].clone_from_slice(bytes);
+    // configure SPI and pins for display
+    let sck = gpiob.pb3.into_af5(&mut gpiob.moder, &mut gpiob.afrl);
+    let miso = gpiob.pb4.into_af5(&mut gpiob.moder, &mut gpiob.afrl); // not used because display does not send data
+    let mosi = gpiob.pb5.into_af5(&mut gpiob.moder, &mut gpiob.afrl);
+    let mut disp_reset = gpioa.pa8.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
+    let dc = gpiob.pb10.into_push_pull_output(&mut gpiob.moder, &mut gpiob.otyper);
+    let disp_chip_select = gpioa.pa10.into_push_pull_output(&mut gpioa.moder, &mut gpioa.otyper);
 
-      let mut tx_slice = &tx_buffer[..bytes.len()]; // take only the part of the buffer with values
-      
-      loop {
-            let sending = tx.write_all(tx_slice, tx_channel);
-            // do other stuff, data is being sent in the background
-            // ownership has to be returned -> wait until the data transfer is finished
-            let (tx_slice_ret, tx_channel_ret, tx_ret) = sending.wait();
-            tx = tx_ret;
-            tx_channel = tx_channel_ret;
-            tx_slice = tx_slice_ret;
-            asm::delay(4_000_000);
-      }
+    let spi: Spi<pac::SPI1, (stm32f3xx_hal::gpio::gpiob::PB3<stm32f3xx_hal::gpio::AF5>, stm32f3xx_hal::gpio::gpiob::PB4<stm32f3xx_hal::gpio::AF5>, stm32f3xx_hal::gpio::gpiob::PB5<stm32f3xx_hal::gpio::AF5>), u8> = Spi::spi1(
+        dp.SPI1,
+        (sck, miso, mosi), 
+        Mode{
+            polarity: embedded_hal::spi::Polarity::IdleLow,
+            phase: embedded_hal::spi::Phase::CaptureOnFirstTransition,
+        },
+        8.mhz(),
+        clocks,
+        &mut rcc.apb2,
+    );
+
+    let mut delay = Delay::new(cp.SYST, clocks);
+
+    // objects for ADC
+    let mut adc_in_pin = gpioa.pa0.into_analog(&mut gpioa.moder, &mut gpioa.pupdr); // PA0
+    
+    // setup adc
+    let mut adc1 = hal::adc::Adc::adc1(
+        dp.ADC1,
+        &mut dp.ADC1_2,
+        &mut rcc.ahb,
+        hal::adc::CkMode::default(),
+        clocks
+    );
+    
+    // create objects to intercat with hardware
+    let mut display = OLEDDisplay::new(spi, dc, disp_chip_select, &mut disp_reset, &mut delay);
+    asm::delay(1_000_000);
+    display.show_text("Start");
+
+    let mut led_bar = LEDBar::new(i2c, I2C_ADDR);
+    led_bar.write_percentage(50).unwrap();
+
+    loop {
+        let adc_in_data: u16 = adc1.read(&mut adc_in_pin).expect("Error reading from ADC");
+        let percentage = (((adc_in_data as f32) / (((0x1 << 12) - 1) as f32) * 100_f32) + 0.5_f32) as u8; // 12 bit ADC
+        led_bar.write_percentage(percentage).unwrap();
+        let mut value_string = String::<30>::from(percentage);
+        value_string.push('%').unwrap();
+        display.show_text(value_string.as_str());
+        asm::delay(1_000_000);
+    }
 }
